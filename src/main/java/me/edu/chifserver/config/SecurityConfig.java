1a572b747bdbcbf03bbeb25b4abe57359e1d0f4b
Esdras Silva <esdraslimasilva83@gmail.com>
2024-04-06 12:12:47 -0300

    Remove useless authentication provider and siable CSRF

--- src/main/java/me/edu/chifserver/config/SecurityConfig.java
+++ src/main/java/me/edu/chifserver/config/SecurityConfig.java
@@ -1,32 +1,25 @@
 package me.edu.chifserver.config;
 
-import me.edu.chifserver.security.FirebaseAuthenticationProvider;
 import me.edu.chifserver.security.filters.FirebaseFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.AuthenticationFilter;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
-
-    @Autowired
-    FirebaseAuthenticationProvider firebaseAuthenticationProvider;
-
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .addFilterBefore(new FirebaseFilter(), BasicAuthenticationFilter.class)
-                .authenticationProvider(firebaseAuthenticationProvider)
-                .authorizeHttpRequests(req -> req.anyRequest().authenticated());
+                .addFilterBefore(new FirebaseFilter(), UsernamePasswordAuthenticationFilter.class)
+                .authorizeHttpRequests(req -> req.anyRequest().authenticated())
+                .csrf(AbstractHttpConfigurer::disable);
 
         return http.build();
     }
